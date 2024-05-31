40fa8b95601945f17a678fbabfd33f1c4212a191
nguyentruongan1 <nguyentruongan1ag@gmail.com>
2024-05-31 18:18:59 +0700

    Route commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/index';
+import Following from './Pages/Following';
+import Profile from './Pages/Profile';
+import Upload from './Pages/Upload';
+import DefaultLayout from './components/Layout/DefaultLayout';
+import HeaderOnly from './components/Layout/HeaderOnly';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <DefaultLayout>
+                            <Home />
+                        </DefaultLayout>
+                    }
+                />
+                <Route path="/following" element={<Following />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route
+                    path="/upload"
+                    element={
+                        <HeaderOnly>
+                            <Upload />
+                        </HeaderOnly>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
